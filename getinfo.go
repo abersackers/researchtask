bb552c9071615c719fb463088c935e3228b1e88c
Abhishek Shah <eabhishekshah@gmail.com>
2020-08-11 22:07:01 -0700

    implemeted cdf and began parallelization attempt

--- getinfo.go
+++ getinfo.go
@@ -4,6 +4,7 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"fmt"
 	"github.com/google/uuid"
 	"io/ioutil"
 	"log"
@@ -11,11 +12,13 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
 //has been tested and properly written csv file for smallLinks.txt
 func main() {
+
 	//read in txt file and run the analysis
 	websites, err := readLines(os.Args[1])
 	if err != nil {
@@ -53,21 +56,37 @@ func csvCreate(websites []string) {
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
+	//input titles into csv for dataframes later
 	writer.Write([]string{"Request ID", "URL Queried", "Query Reponse Status", "Start Time", "End Time", "Response Size"})
 
+	//implement parallelism
+	var waitGroup sync.WaitGroup
+
+	// Give some time for listenForever to start
+	time.Sleep(time.Nanosecond * 10)
+
 	for i := 0; i < len(websites); i++ {
-		requestID, urlQueried, respStatus, startTime, endTime, respSize := httpData(websites[i])
-		data := []string{requestID.String(), urlQueried, respStatus, strconv.Itoa(int(startTime)), strconv.Itoa(int(endTime)), strconv.Itoa(int(respSize))}
-		writer.Write(data)
+		//parallelize for loop by adding process and adding go func (i)
+		waitGroup.Add(1)
+
+		go func(i int) {
+			// At the end of the goroutine, tell the WaitGroup
+			//   that another thread has completed.
+			defer waitGroup.Done()
+			requestID, urlQueried, respStatus, startTime, endTime, respSize := httpData(websites[i])
+			if respStatus != "false" {
+				data := []string{requestID.String(), urlQueried, respStatus, strconv.Itoa(int(startTime)), strconv.Itoa(int(endTime)), strconv.Itoa(int(respSize))}
+				writer.Write(data)
+			}
+		}(i)
 	}
 
+	//wait for this parallel process to end before exiting the function
+	waitGroup.Wait()
 }
 
 //analysis code of each website
-//goal: figure out response bytes
-//timing out on largeLinks.txt, current iteration is on github
-//seems to be a timeout issue which will require further investigation, however
-//since it works well on smallLinks.txt will come back later
+//goal: figure out why parallelism isn't working on largeLinks
 func httpData(website string) (uuid.UUID, string, string, int64, int64, float64) {
 
 	requestID := uuid.New() //randomly generate locally since it does not need to be sent in network request
@@ -78,17 +97,18 @@ func httpData(website string) (uuid.UUID, string, string, int64, int64, float64)
 
 	resp, err := http.Get(website)
 	if err != nil {
+		//use these different formats to see if there is a possible solution
 		if strings.HasPrefix(website, "https://") && !(strings.HasPrefix(website, "https://www.")) {
+			//https:// failed try https://www.
 			return httpData("https://www." + website[8:len(website)])
 		} else if strings.HasPrefix(website, "https://www.") {
+			//https:// failed try http://
 			return httpData("http://" + website[12:len(website)])
 		} else if strings.HasPrefix(website, "http://") && !strings.HasPrefix(website, "http://www.") {
+			//http:// failed try http://www.
 			return httpData("http://www." + website[7:len(website)])
-		} else if strings.HasPrefix(website, "http://www.") {
-			return httpData("www." + website[11:len(website)])
-		} else if strings.HasPrefix(website, "www.") {
-			return httpData(website[4:len(website)])
 		}
+		// indicated failure and will not be added to csv
 		return requestID, urlQueried, "false", startTime, time.Now().UnixNano(), -1
 	}
 	defer resp.Body.Close()
@@ -96,10 +116,13 @@ func httpData(website string) (uuid.UUID, string, string, int64, int64, float64)
 	endTime := time.Now().UnixNano()
 
 	//calculate response statistics
-	//fmt.Println(resp.Status)
 	respStatus := resp.Status
 	body, err := ioutil.ReadAll(resp.Body)
+
+	//need to figure out better error handling here and why concurrency fails at this step
 	if err != nil {
+		fmt.Println("error: " + website)
+		httpData(website)
 		panic(err)
 	}
 	respSize := len(body) //interpreted as number of bytes from io
--- visualizations.py
+++ visualizations.py
@@ -1,1 +1,45 @@
+# CSV analysis in python
+# Using pandas and numpy, takes in a csv file that is outputted from getinfo.go
+# and prints average bytes per get request as well as calculating the cdf
+# and printing some percentiles of interests
 
+import pandas as pd
+import numpy as np
+
+
+# takes in a pandas dataframe with the assumption that the 5th indexed
+# entry is the total bytes of the get request and calculates an average
+# of the total bytes of every entry
+def calculate_average_bytes(df):
+    total_entries = 0
+    total_bytes = 0
+    for index, row in df.iterrows():
+        total_entries += 1
+        total_bytes += row[5]
+
+    return 1.0 * total_bytes / total_entries
+
+
+# Calculate cdf using given function from Sudhessh
+def cdf(data):
+    n = len(data)
+    x = np.sort(data)  # sort your data
+    y = np.arange(1, n + 1) / n  # calculate cumulative probability
+    return x, y
+
+
+# Using an array of percentiles of interest, print out the associated value
+# of that percentile from an inputted cdf
+def print_percentiles(sorted):
+    percentiles = [50, 70, 75, 80, 85, 90, 95, 99]
+    for i in percentiles:
+        print(str(i) + "th percentile is: " + str(np.percentile(sorted, i)))
+
+
+# read the csv output of getinfo.go
+data = pd.read_csv("requestData.csv")
+
+# calculate & print percentile data for difference in time
+agg_data = pd.array(data['End Time'] - data['Start Time']).to_numpy()
+sorted, cdf = cdf(agg_data)
+print_percentiles(sorted)
